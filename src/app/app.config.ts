44fe5c5579691266d8dae15f480c987550188a1c
jeffr63 <jeffr63@gmail.com>
2023-04-11 07:55:31 -0500

    Use new ApplicationConfig bootstrap method

---
+++ src/app/app.config.ts
@@ -0,0 +1,32 @@
+import { ApplicationConfig } from '@angular/platform-browser';
+import { importProvidersFrom } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
+import { TitleStrategy, provideRouter } from '@angular/router';
+
+import { NgxsDispatchPluginModule } from '@ngxs-labs/dispatch-decorator';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsModule } from '@ngxs/store';
+
+import { APP_ROUTES } from './app.routes';
+import { CustomTitleStrategyService } from './services/custom-title-strategy.service';
+import { CoursesState } from './state/course/course.state';
+import { SourcesState } from './state/sources/sources.state';
+import { PathsState } from './state/paths/paths.state';
+import { UsersState } from './state/users/users.state';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    importProvidersFrom(
+      NgxsModule.forRoot([CoursesState, SourcesState, PathsState, UsersState], {
+        selectorOptions: { suppressErrors: false, injectContainerState: false },
+      }),
+      NgxsDispatchPluginModule.forRoot(),
+      NgxsReduxDevtoolsPluginModule.forRoot()
+    ),
+    { provide: TitleStrategy, useClass: CustomTitleStrategyService },
+    provideAnimations(),
+    provideHttpClient(),
+    provideRouter(APP_ROUTES),
+  ],
+};
--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient } from '@angular/common/http';
-import { provideRouter, TitleStrategy } from '@angular/router';
-
-import { NgxsModule } from '@ngxs/store';
-import { NgxsDispatchPluginModule } from '@ngxs-labs/dispatch-decorator';
-import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { enableProdMode } from '@angular/core';
 
 import { environment } from './environments/environment';
-import { APP_ROUTES } from './app/app.routes';
 import { AppComponent } from './app/app.component';
-import { CustomTitleStrategyService } from './app/services/custom-title-strategy.service';
-import { CoursesState } from './app/state/course/course.state';
-import { PathsState } from './app/state/paths/paths.state';
-import { SourcesState } from './app/state/sources/sources.state';
-import { UsersState } from './app/state/users/users.state';
+import { appConfig } from './app/app.config';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    importProvidersFrom(
-      NgxsModule.forRoot([CoursesState, SourcesState, PathsState, UsersState], {
-        selectorOptions: { suppressErrors: false, injectContainerState: false },
-      }),
-      NgxsDispatchPluginModule.forRoot(),
-      NgxsReduxDevtoolsPluginModule.forRoot()
-    ),
-    { provide: TitleStrategy, useClass: CustomTitleStrategyService },
-    provideAnimations(),
-    provideHttpClient(),
-    provideRouter(APP_ROUTES),
-  ],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
