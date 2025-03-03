ef7399971d462191b3687277110bc8d88775f21f
jeffr63 <jeffr63@gmail.com>
2025-03-03 07:57:43 -0600

    Update source data service test cases

--- src/app/shared/services/source/source-data.service.spec.ts
+++ src/app/shared/services/source/source-data.service.spec.ts
@@ -1,55 +1,50 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
+import { DataService } from '@services/common/data.service';
 import { Source } from '@models/sources';
 import { SourceDataService } from './source-data.service';
+import { of } from 'rxjs';
 
-const baseUrl = 'http://localhost:3000';
+const baseUrl = 'http://localhost:3000/sources';
 
 describe('SourcesService', () => {
-  let httpTestingController: HttpTestingController;
   let service: SourceDataService;
+  let dataServiceSpy: jasmine.SpyObj<DataService>;
 
   beforeEach(() => {
+    const spy = jasmine.createSpyObj('DataService', ['add', 'delete', 'getById', 'getAll', 'update']);
     TestBed.configureTestingModule({
       imports: [],
-      providers: [SourceDataService, provideHttpClient(withInterceptorsFromDi()), provideHttpClientTesting()],
+      providers: [SourceDataService, { provide: DataService, useValue: spy }],
     });
 
-    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(SourceDataService);
+    dataServiceSpy = TestBed.inject(DataService) as jasmine.SpyObj<DataService>;
   });
 
   describe('deleteSource', () => {
     it('should return deleted source with a delete call to the correct URL', () => {
       const source = { id: 1, name: 'ABC' };
+      dataServiceSpy.delete.and.returnValue(of(source));
 
       service.deleteSource(1).subscribe((data: Source) => {
+        expect(dataServiceSpy.delete).toHaveBeenCalledWith(1, baseUrl);
         expect(data.id).toBe(1);
         expect(data).toEqual(source);
       });
-
-      const req = httpTestingController.expectOne(`${baseUrl}/sources/1`);
-      req.flush(source);
-      expect(req.request.method).toBe('DELETE');
-      httpTestingController.verify();
     });
   });
 
   describe('getSource', () => {
     it('should return requested source with a get call to the correct URL', () => {
       const source = { id: 1, name: 'ABC' };
+      dataServiceSpy.getById.and.returnValue(of(source));
 
       service.getSource(1).subscribe((data: Source) => {
+        expect(dataServiceSpy.getById).toHaveBeenCalledWith(1, baseUrl);
         expect(data.id).toBe(1);
         expect(data).toEqual(source);
       });
-
-      const req = httpTestingController.expectOne(`${baseUrl}/sources/1`);
-      req.flush(source);
-      expect(req.request.method).toBe('GET');
-      httpTestingController.verify();
     });
   });
 
@@ -59,32 +54,26 @@ describe('SourcesService', () => {
         { id: 1, name: 'ABC' },
         { id: 2, name: 'DEF' },
       ];
+      dataServiceSpy.getAll.and.returnValue(of(sources));
 
       service.loadSources().subscribe((data: Source[]) => {
+        expect(dataServiceSpy.getAll).toHaveBeenCalledWith(`${baseUrl}?_sort=name&_order=asc`);
         expect(data.length).toBe(2);
         expect(data).toEqual(sources);
       });
-
-      const req = httpTestingController.expectOne(`${baseUrl}/sources?_sort=name&_order=asc`);
-      req.flush(sources);
-      expect(req.request.method).toBe('GET');
-      httpTestingController.verify();
     });
   });
 
   describe('saveSource, with id', () => {
     it('should return requested source with a put call to the correct URL', () => {
       const source = { id: 1, name: 'ABC' };
+      dataServiceSpy.update.and.returnValue(of(source));
 
       service.saveSource(source).subscribe((data: Source) => {
+        expect(dataServiceSpy.update).toHaveBeenCalledWith(1, source, baseUrl);
         expect(data.id).toBe(1);
         expect(data).toEqual(source);
       });
-
-      const req = httpTestingController.expectOne(`${baseUrl}/sources/1`);
-      req.flush(source);
-      expect(req.request.method).toBe('PUT');
-      httpTestingController.verify();
     });
   });
 
@@ -92,16 +81,13 @@ describe('SourcesService', () => {
     it('should return requested source with a post call to the correct URL', () => {
       const source = { id: null, name: 'ABC' };
       const returns = { id: 1, name: 'ABC' };
+      dataServiceSpy.add.and.returnValue(of(returns));
 
       service.saveSource(source).subscribe((data: Source) => {
+        expect(dataServiceSpy.add).toHaveBeenCalledWith(source, baseUrl);
         expect(data.id).toBe(1);
         expect(data).toEqual(returns);
       });
-
-      const req = httpTestingController.expectOne(`${baseUrl}/sources`);
-      req.flush(returns);
-      expect(req.request.method).toBe('POST');
-      httpTestingController.verify();
     });
   });
 });
